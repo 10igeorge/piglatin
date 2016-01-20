78454aa31b31015851d8ad0c744012c446485546
Daren Schaad and Isabelle George <darenschaad@gmail.com>
2016-01-20 09:47:07 -0800

    add consonant functionality for pig latin translator

--- Desktop/week3wed/piglatin/js/scripts.js
+++ Desktop/week3wed/piglatin/js/scripts.js
@@ -1,5 +1,15 @@
+
 var vowel = function(word) {
-  if (word[0] === "a" || "e" || "i" || "o" || "u") {
+  // var vowels = ["a", "e", "i", "o", "u"]
+  if(word[0] ==="a" || word[0] ==="e" || word[0] ==="i" || word[0] ==="o" || word[0] ==="u") {
     return (word + "ay");
+  };
+};
+
+var consonant = function(word) {
+  // var firstLetter = word.charAt(0);
+  // var secondLetter = word.charAt(1)
+  if ((word[0] !== "a" || word[0] !=="e" || word[0] !=="i" || word[0] !=="o" || word[0] !=="u") && (word[1] !=="a" || word[1] !=="e" || word[1] !=="i" || word[1] !=="o" || word[1] !=="u")) {
+    return word.slice(2) + word.slice(0,2) + "ay";
   };
 };
--- Desktop/week3wed/piglatin/specs/specs.js
+++ Desktop/week3wed/piglatin/specs/specs.js
@@ -1,5 +1,11 @@
 describe('vowel', function(){
     it("adds 'ay' to the end of words that start with a vowel", function(){
-      expect(vowel("apple")).to.equal("appleay");
+      expect(vowel("orange")).to.equal("orangeay");
+    });
+});
+
+describe('consonant', function(){
+    it("takes all consonants before the first vowel in the word and moves them to the back of the word and add ay subsequently", function(){
+      expect(consonant("tray")).to.equal("aytray");
     });
 });
